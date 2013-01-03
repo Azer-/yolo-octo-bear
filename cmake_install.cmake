9168c8534dce2e7d94095432e555701fa7dbc5f8
Adrien Delorme <azer@sky>
2013-01-03 17:01:53 +0100

    Cleanup

--- cmake_install.cmake
+++ cmake_install.cmake
@@ -1,4 +1,4 @@
-# Install script for directory: /home/niko/Projects/ParallelComp_Project
+# Install script for directory: /home/azer/projects/yolo-octo-bear
 
 # Set the install prefix
 IF(NOT DEFINED CMAKE_INSTALL_PREFIX)
@@ -38,7 +38,7 @@ ELSE(CMAKE_INSTALL_COMPONENT)
   SET(CMAKE_INSTALL_MANIFEST "install_manifest.txt")
 ENDIF(CMAKE_INSTALL_COMPONENT)
 
-FILE(WRITE "/home/niko/Projects/ParallelComp_Project/${CMAKE_INSTALL_MANIFEST}" "")
+FILE(WRITE "/home/azer/projects/yolo-octo-bear/${CMAKE_INSTALL_MANIFEST}" "")
 FOREACH(file ${CMAKE_INSTALL_MANIFEST_FILES})
-  FILE(APPEND "/home/niko/Projects/ParallelComp_Project/${CMAKE_INSTALL_MANIFEST}" "${file}\n")
+  FILE(APPEND "/home/azer/projects/yolo-octo-bear/${CMAKE_INSTALL_MANIFEST}" "${file}\n")
 ENDFOREACH(file)
